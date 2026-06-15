157ecda0b40b8d5103b70185d8ce92dccb85f8ab
darioguayasamin779-commits <darioguayasamin779@gmail.com>
2026-06-15 07:13:55 -0500

    Mejore la carpeta para que solo haya dos archivos, asi el archivo zapats_padre ya no existe y agregue validaciones

--- zapatos_devoluciones/models/zapatos_garantia.py
+++ zapatos_devoluciones/models/zapatos_garantia.py
@@ -1,10 +1,11 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class Garantia(models.Model):
     _name = 'zapateria.garantia'
     _description = 'Garantías'
 
-    codigo = fields.Char(string='Código')
+    codigo = fields.Char(string='Código',readonly=True, default='Nuevo')
 
     fecha_inicio = fields.Date(string='Fecha Inicio')
 
@@ -19,3 +20,24 @@ class Garantia(models.Model):
         string='Estado')
 
     zapato_id = fields.Many2one('zapatos.zapato', string='Zapato',required=True)
+
+    _sql_constraints = [
+        (
+            'codigo_garantia_unico',
+            'unique(codigo)',
+            'Ya existe una garantía con ese código.'
+        )
+    ]
+
+
+    @api.constrains('fecha_inicio', 'fecha_fin')
+    def _check_fechas(self):
+        for record in self:
+            if record.fecha_fin and record.fecha_inicio:
+                if record.fecha_fin < record.fecha_inicio:
+                    raise ValidationError("La fecha final no puede ser menor a la fecha inicial")
+    @api.constrains('descripcion')
+    def _check_descripcion(self):
+        for record in self:
+            if not record.descripcion or len(record.descripcion.strip()) < 10:
+                raise ValidationError("La descripción debe tener al menos 10 caracteres.")
