829cc5d01d0c9e988ba898e000579c69a73b166c
Chrisx-cmd <castanedachristian2411@gmail.com>
2026-06-16 08:10:37 -0500

    Corrijo la vista de zapatos_extension y el .py

--- zapatos_extension/models/zapato_extension.py
+++ zapatos_extension/models/zapato_extension.py
@@ -3,22 +3,38 @@ from odoo import models, fields, api
 class ZapatosZapato(models.Model):
     _inherit = 'zapatos.zapato'
 
-    codigo = fields.Char(string='Còdigo')
+    codigo = fields.Char(string='Codigo')
     marca = fields.Char(string='Marca')
     color = fields.Char(string='Color')
-    material = fields.Char (string='Material')
-    descripcion = fields.Text(string = 'Descripciòn')
-    stock_minimo = fields.Integer(string = 'Stock Minimo', default = 5)
-
+    material = fields.Char(string='Material')
+    descripcion = fields.Text(string='Descripcion')
+    stock_minimo = fields.Integer(string='Stock Minimo', default=5)
+    talla = fields.Selection([
+        ('35', '35'),
+        ('36', '36'),
+        ('37', '37'),
+        ('38', '38'),
+        ('39', '39'),
+        ('40', '40'),
+        ('41', '41'),
+        ('42', '42'),
+        ('43', '43'),
+        ('44', '44'),
+        ('45', '45'),
+    ], string='Talla')
+    temporada = fields.Selection([
+        ('verano', 'Verano'),
+        ('invierno', 'Invierno'),
+        ('primavera', 'Primavera'),
+        ('otono', 'Otoño'),
+    ], string='Temporada')
     valor_inventario = fields.Float(
-        string ='Valor_inventario',
-        compute = '_compute_valor_inventario',
-        store = True
+        string='Valor Inventario',
+        compute='_compute_valor_inventario',
+        store=True
     )
 
-    @api.depends('precio','stock')
+    @api.depends('precio', 'stock')
     def _compute_valor_inventario(self):
         for record in self:
-            record.valor_inventario = record.precio * record.stock 
-
-            
+            record.valor_inventario = record.precio * record.stock
